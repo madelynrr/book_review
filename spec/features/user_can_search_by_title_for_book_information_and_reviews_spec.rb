f34c4f93f2d81d4d16bbaf85bc856a950145bde8
Madelyn Romero <m.romero013@gmail.com>
2020-02-21 09:23:06 -0700

    Begin test for information and reviews by title

---
+++ spec/features/user_can_search_by_title_for_book_information_and_reviews_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "as a user" do
+  it "can enter a book title and get back the information and New York Times reviews for the book" do
+    
+
+  end
+end
+
+# As a user
+# When I visit "/"
+# And I input "the man who saw everything" into the form
+# (Note: Use the existing search form)
+# And I click "Find Book"
+# Then I should be on page "/search"
+# Then I should see the book's info
+# For that book I should see
+# - Title
+# - Author
+# - Genres
+# (Note: genres is referred to as "subjects" in the book search response)
+# I should also see:
+# - This book has 2 reviews from the New York Times
+#   (Note: reviews are the "summary" in the book review response)
+# - Review 1: ""The Man Who Saw Everything,"" which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at "authoritarian old men.""
+# Review Publication Date: 2019-10-09
+# - Review 2: "Deborah Levy's latest novel, "The Man Who Saw Everything," experiments with time travel, history and the endless complications of love."
+# Review Publication Date: 2019-10-15
